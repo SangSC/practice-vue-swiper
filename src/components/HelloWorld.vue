e34cb9e5c6c229f218ffd9d2eea5245eea4f6c15
SangSC <r.sang.chao@gmail.com>
2024-03-03 01:12:24 +0800

    initial

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,57 +1,93 @@
-<template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
-</template>
-
 <script>
+// Import Swiper Vue.js components
+import { Swiper, SwiperSlide } from "swiper/vue";
+
+// import required modules
+import { Pagination } from "swiper/modules";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+// import "./style.css";
+
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  components: {
+    Swiper,
+    SwiperSlide,
+  },
+  setup() {
+    const photos = [
+      "photo_1.jpg",
+      "photo_2.jpg",
+      "photo_3.jpg",
+      "photo_4.jpg",
+      "photo_5.jpg",
+      "photo_6.jpg",
+      "photo_7.jpg",
+      "photo_8.jpg",
+    ];
+    return {
+      modules: [Pagination],
+      photos,
+    };
+  },
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
+<template>
+  <main class="container">
+    <h1>Photo Gallery</h1>
+    <Swiper
+      :loop="true"
+      :spaceBetween="30"
+      :pagination="{
+        clickable: true,
+        dynamicBullets: true,
+      }"
+      :modules="modules"
+      class="swiper"
+    >
+      <SwiperSlide v-for="photo in photos" :key="photo">
+        <img :src="`${photo}`" alt="" />
+      </SwiperSlide>
+    </Swiper>
+  </main>
+</template>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300..900;1,300..900&display=swap");
+
+* {
+  margin: 0;
   padding: 0;
+  box-sizing: border-box;
+  font-family: "Rubik", sans-serif;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+body {
+  background-color: #b2b2c6;
+  color: aliceblue;
 }
-a {
-  color: #42b983;
+
+.container {
+  margin: 0 auto;
+  padding: 50px 0;
+  max-width: 1000px;
+}
+
+.swiper {
+  width: 800px;
+  height: 600px;
+  overflow: visible;
+}
+
+.swiper-slide img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+.swiper-pagination-bullets {
+  width: 12px;
+  height: 12px;
 }
 </style>
